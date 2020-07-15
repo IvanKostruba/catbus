4cdd6081c14be909369e3cd720bf36531e53df00
Ivan <ivan.kostruba@crosscard.com>
2020-07-15 17:54:15 +0200

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -5,6 +5,10 @@
 #include "event_sender.h"
 #include "worker_mutex.h"
 
+#include <iostream>
+
+using namespace std::chrono_literals;
+
 // Events
 struct Init
 {};
@@ -27,7 +31,15 @@ class Sender : public catbus::EventSender<Request>
 public:
   explicit Sender(size_t id) : id_{id}
   {}
-  void Handle(Init event) { Send(Request{id_}); }
+  void Handle(Init event)
+  { 
+    std::cout << "Init received\n";
+    Send( Request{id_} );
+  }
+  void Handle(Response event)
+  { 
+    std::cout << "Response received: code " << event.error_code << "\n";
+  }
   
   const size_t id_;
 };
@@ -35,14 +47,22 @@ public:
 class Receiver : public catbus::EventSender<Response>
 {
 public:
-  void Handle(Request req) { Send(Response{req.sender, 200}); }
+  void Handle(Request req)
+  { 
+    std::cout << "Request received: " << req.data << "\n";
+    Send(Response{req.sender, 200});
+  }
 };
 
-// Initialization
-catbus::EventBus<WorkerUnitMutex> bus{2}; // The threads where handlers will run
-Sender sender{1};
-Receiver receiver;
-catbus::setup_dispatch(bus, sender, receiver); // Setting up Send() methods
+int main(int argc, char** argv) {
+  // Initialization
+  catbus::EventCatbus<catbus::WorkerUnitMutex> bus{2}; // The threads where handlers will run
+  Sender sender{1};
+  Receiver receiver;
+  catbus::setup_dispatch(bus, sender, receiver); // Setting up Send() methods
 
-// Startup
-catbus::static_dispatch(bus, Init{}, sender); // Send the initial event
+  // Startup
+  catbus::static_dispatch(bus, Init{}, sender); // Send the initial event
+  std::this_thread::sleep_for(200ms);
+}
+
